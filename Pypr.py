20907f6f7aae524e8ff78446b4d6b51c63c376fe
Jason Stitt <jason@countergram.com>
2014-08-08 23:31:45 -0500

    Changed from text input to search quick panel

--- Pypr.py
+++ Pypr.py
@@ -23,6 +23,11 @@ def fix_path():
     PATH = subprocess.check_output(command, shell=True).decode('utf8')
 
 
+def env():
+    " Return an environment to pass to subprocess "
+    return {"PATH": PATH} if PATH else None
+
+
 class open_module(sublime_plugin.WindowCommand):
 
     " Prompt for a package name and attempt to load the file or dir "
@@ -30,12 +35,13 @@ class open_module(sublime_plugin.WindowCommand):
     py2 = False
 
     def run(self):
-        self.window.show_input_panel(
-            "Package name", "", self.input_done, None, None)
+        self.modules = get_module_list(self.py2)
+        self.window.show_quick_panel(self.modules, self.input_done)
 
-    def input_done(self, text):
-        text = text.splitlines()[0]
-        text = non_alphanum.sub('', text)
+    def input_done(self, index):
+        if index == -1:
+            return
+        text = self.modules[index]
         try:
             module_path = find_module_path(text, self.py2)
             open_in_new_window(module_path)
@@ -47,13 +53,18 @@ class open_module_py2(open_module):
     py2 = True
 
 
+find_module_path_script = r"""\
+import imp
+print(imp.find_module('{}')[1])
+"""
+
+
 def find_module_path(text, py2=False):
     " Try to find the filesystem location of a Python module. "
-    env = {"PATH": PATH} if PATH else None
     module_path = subprocess.check_output([
         "python2" if py2 else "python3",
         "-c",
-        "import imp;print(imp.find_module('{}')[1])".format(text)], env=env)
+        find_module_path_script.format(text)], env=env())
     return module_path.decode('utf8').strip()
 
 
@@ -64,6 +75,32 @@ def open_in_new_window(path):
     if sublime.platform() == 'osx':
         app_path = executable_path[:executable_path.rfind(".app/") + 5]
         executable_path = app_path + "Contents/SharedSupport/bin/subl"
+    subprocess.Popen([executable_path, path], env=env())
 
-    env = {"PATH": PATH} if PATH else None
-    subprocess.Popen([executable_path, path], env=env)
+
+get_module_list_script = r"""\
+import pkgutil
+import os
+
+valid_names = [
+    name for (loader, name, is_pkg) in pkgutil.iter_modules()
+    if os.path.isfile(os.path.join(loader.path, name) + ".py")
+    or os.path.isdir(os.path.join(loader.path, name))
+]
+
+print("\n".join(valid_names))
+"""
+
+
+def get_module_list(py2=False):
+    " List of names of available modules for an interpreter "
+    raw_output = subprocess.check_output([
+        "python2" if py2 else "python3",
+        "-c",
+        get_module_list_script], env=env())
+    module_names = [
+        line.decode('utf8').strip()
+        for line in raw_output.splitlines() if line]
+    # Sort names case-insensitive and underbar-names to bottom
+    module_names.sort(key=str.upper)
+    return module_names
